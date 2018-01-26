cb863dc04ab3ff2fd48ab750eb7717411f74ca0d
coreyminess <fiveandpine@gmail.com>
2018-01-26 09:56:52 -0800

    Updating app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,8 @@
-'use strict';
-
 console.log('app.js is running');
 
+
 //JSX
-var template = React.createElement(
-  'p',
-  null,
-  'This is JSX from app.js'
-);
+var template = <p>This is JSX from app.js</p>;
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
