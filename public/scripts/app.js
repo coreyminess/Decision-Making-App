d10e4fa7788a346e09d9502ffa6eec8b385c36cc
coreyminess <fiveandpine@gmail.com>
2018-01-30 12:54:39 -0800

    Adding ESLint / Changing array logic to update / re-render when objects submitted to formfield

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,94 +7,80 @@ console.log('app.js is running');
 var app = {
     title: 'Indecision app',
     subtitle: 'Poopy',
-    options: ['One', 'Two']
+    options: []
 };
 
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        app.title
-    ),
-    app.subtitle && React.createElement(
-        'p',
-        null,
-        app.subtitle
-    ),
-    React.createElement(
-        'p',
-        null,
-        app.options.length > 0 ? 'Here are your options' : 'No options'
-    ),
-    React.createElement(
-        'p',
-        null,
-        app.options
-    ),
-    React.createElement(
-        'ol',
-        null,
-        React.createElement(
-            'li',
-            null,
-            'Item one'
-        ),
-        React.createElement(
-            'li',
-            null,
-            'Item two'
-        )
-    )
-);
+var onFormSubmit = function onFormSubmit(e) {
+    //prevent full page refresh
+    e.preventDefault();
 
-var count = 0;
-var addOne = function addOne() {
-    count++;
-    renderCounterApp();
+    var option = e.target.elements.option.value;
+    //ignore form submission until something is inputted. Add inputs to array.
+
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = ', ';
+        renderCounter();
+    }
 };
 
-var minusOne = function minusOne() {
-    count--;
-    renderCounterApp();
-};
-
-var resetButton = function resetButton() {
-    count = 0;
-    renderCounterApp();
-};
-
-var appRoot = document.getElementById('app');
-
-var renderCounterApp = function renderCounterApp() {
-    var templateTwo = React.createElement(
+var renderCounter = function renderCounter() {
+    var template = React.createElement(
         'div',
         null,
         React.createElement(
             'h1',
             null,
-            'Count: ',
-            count
+            app.title
+        ),
+        app.subtitle && React.createElement(
+            'p',
+            null,
+            app.subtitle
         ),
         React.createElement(
-            'button',
-            { onClick: addOne },
-            '+1'
+            'p',
+            null,
+            app.options.length > 0 ? 'Here are your options' : 'No options'
         ),
         React.createElement(
-            'button',
-            { onClick: minusOne },
-            '-1'
+            'p',
+            null,
+            app.options
         ),
         React.createElement(
-            'button',
-            { onClick: resetButton },
-            'Reset'
+            'p',
+            null,
+            app.options.length
+        ),
+        React.createElement(
+            'ol',
+            null,
+            React.createElement(
+                'li',
+                null,
+                'Item one'
+            ),
+            React.createElement(
+                'li',
+                null,
+                'Item two'
+            )
+        ),
+        React.createElement(
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option' }),
+            React.createElement(
+                'button',
+                null,
+                'Add Option'
+            )
         )
     );
 
-    ReactDOM.render(templateTwo, appRoot);
+    ReactDOM.render(template, appRoot);
 };
 
-renderCounterApp();
+var appRoot = document.getElementById('app');
+renderCounter();
--- src/app.js
+++ src/app.js
@@ -1,59 +1,49 @@
 console.log('app.js is running');
 
-
-
 //JSX
 
 const app = {
     title: 'Indecision app',
     subtitle: 'Poopy',
-    options: ['One', 'Two']
+    options: []
+};
+
+const onFormSubmit = (e) => {
+    //prevent full page refresh
+    e.preventDefault();
+
+    const option = e.target.elements.option.value;
+    //ignore form submission until something is inputted. Add inputs to array.
+
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = ', ';
+        renderCounter();
+    }
 };
 
 
-const template = (
-    <div>
-        <h1>{app.title}</h1>
-        {app.subtitle && <p>{app.subtitle}</p>}
-        <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-        <p>{app.options}</p>
-        <ol>
-           <li>Item one</li>
-           <li>Item two</li>
-        </ol>
-    </div>
-);
-
-let count = 0;
-const addOne = () => {
-    count++;
-    renderCounterApp();
-};
-
-const minusOne = () => {
-    count--;
-    renderCounterApp();
-};
-
-const resetButton = () => {
-    count = 0;
-    renderCounterApp();
-};
-
-const appRoot = document.getElementById('app');
-
-
-const renderCounterApp = () => {
-    const templateTwo = (
+const renderCounter = () => {
+    const template = (
         <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={minusOne}>-1</button>
-            <button onClick={resetButton}>Reset</button>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+            <p>{app.options}</p>
+            <p>{app.options.length}</p>
+            <ol>
+                <li>Item one</li>
+                <li>Item two</li>
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option"/>
+                <button>Add Option</button>
+            </form>
         </div>
     );
 
-    ReactDOM.render(templateTwo, appRoot);
+    ReactDOM.render(template, appRoot);
 };
 
-renderCounterApp();
+const appRoot = document.getElementById('app');
+renderCounter();
